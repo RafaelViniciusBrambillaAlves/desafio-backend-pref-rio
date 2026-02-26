214c03be33efd9587df2af6aa71a937944b53033
LeonardoBrambilla-oak <leonardo.brambilla@oaktecnologia.com>
2026-02-26 14:52:18 -0300

    fix exceptions

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -3,6 +3,9 @@ from fastapi import HTTPException
 
 class AppException(HTTPException):
     def __init__(self, *, error: str, message: str, status_code: int):
+        self.error = error
+        self.message = message
+        
         super().__init__(
             status_code=status_code,
             detail={
