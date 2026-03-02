863706db353ff7d5681853c519b21f677cf912d0
LeonardoBrambilla-oak <leonardo.brambilla@oaktecnologia.com>
2026-03-02 15:14:05 -0300

    add mock_uow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
+import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from app.main import app
+from unittest.mock import AsyncMock
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def client():
 
     transport = ASGITransport(app = app)
@@ -12,3 +14,10 @@ async def client():
         base_url = "http://test"
     ) as client:
         yield client
+
+@pytest.fixture
+def mock_uow():
+    uow = AsyncMock()
+    uow.__aenter__.return_value = uow
+    uow.__aexit__.return_value = None
+    return uow
